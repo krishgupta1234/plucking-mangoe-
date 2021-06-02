02a24a467f72caac923a0fa837c2e49da8428a7d
krishgupta1234 <75523301+krishgupta1234@users.noreply.github.com>
2021-06-02 20:46:26 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,38 @@
+class launcher {
+    constructor(body, point) {
+        var options = {
+            bodyA: body,
+            pointB: point,
+            stiffness: 0.004,
+            length: 1
+        }
+
+        this.bodyA = body
+        this.pointB = point
+        this.launcher = Constraint.create(options)
+        World.add(world, this.launcher)
+
+    }
+
+fly()
+{
+this.launcher.bodyA=null;
+
+}
+
+display(){
+    if(this.launcher.bodyA){
+
+
+        var pointA = this.bodyA.position
+        var pointB = this.pointB
+strokeWeight(2);
+line(pointA.x,pointA.y,pointB.x,pointB.y)
+}
+
+}
+
+
+
+
+}
